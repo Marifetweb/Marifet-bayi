86eceb9fa0eae4561871d66eb5adae2a3026b680
Marifetweb <ahmedfarukicelik@gmail.com>
2026-04-23 16:57:39 +0300

    Update ProductListing.tsx

--- src/pages/ProductListing.tsx
+++ src/pages/ProductListing.tsx
@@ -607,7 +607,7 @@ export default function ProductListing() {
                     </p>
                   </div>
                 ) : (
-                  <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-3">
+                  <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4 gap-3">
                     {filteredProducts.map((product) => (
                       <div
                         key={product.id}
