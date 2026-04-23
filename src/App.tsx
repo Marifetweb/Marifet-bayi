c68ea5fd62cf2a3e66d248ab074f7f944ddc72be
Marifetweb <ahmedfarukicelik@gmail.com>
2026-04-23 16:09:12 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { Switch, Route } from "wouter";
 import Home from "@/pages/Home";
 import Login from "@/pages/Login";
 import Register from "@/pages/Register";
+import Campaigns from "@/pages/Campaigns";
+import ProductListing from "@/pages/ProductListing";
 import NotFound from "@/pages/NotFound";
 import { AuthProvider } from "@/context/AuthContext";
 
@@ -10,8 +12,12 @@ export default function App() {
     <AuthProvider>
       <Switch>
         <Route path="/" component={Home} />
+        <Route path="/katalog" component={ProductListing} />
+        <Route path="/kampanyalar" component={Campaigns} />
+        <Route path="/login" component={Login} />
         <Route path="/giris" component={Login} />
         <Route path="/kayit" component={Register} />
+        <Route path="/register" component={Register} />
         <Route component={NotFound} />
       </Switch>
     </AuthProvider>
